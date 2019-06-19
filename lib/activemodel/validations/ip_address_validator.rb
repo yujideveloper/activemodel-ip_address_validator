d31e9d1ef75bb377309408bf38d642f982131bd3
Yuji Hanamura <yuji.developer@gmail.com>
2019-06-19 07:56:44 +0000

    Add `validates_ip_address_of` method

--- lib/activemodel/validations/ip_address_validator.rb
+++ lib/activemodel/validations/ip_address_validator.rb
@@ -31,5 +31,11 @@ module ActiveModel
         record.errors.add(attribute, :invalid, options.except(:version).merge!(value: value))
       end
     end
+
+    module HelperMethods
+      def validates_ip_address_of(*attr_names)
+        validates_with IpAddressValidator, _merge_attributes(attr_names)
+       end
+    end
   end
 end
